1e299202324bd0f126846be6b0be992f18c0339e
ashishnaagar0745 <945vm4c8cd@privaterelay.appleid.com>
2026-08-02 09:46:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll",function(){
+const header=document.querySelector("header");
+
+if(window.scrollY>50){
+header.style.background="#000";
+}else{
+header.style.background="rgba(0,0,0,.85)";
+}
+});
+
+console.log("KB Group Website Loaded Successfully");
